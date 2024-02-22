68e1beeb5af0442cff3563de4fbdd059c49397ab
Johannes Könönen <Johannes.Kononen@student.lab.fi>
2024-02-22 14:47:18 +0200

    File exercise

---
+++ FileExercise/Book.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExercise
+{
+    internal class Book
+    {
+        private string title;
+        private string author;
+        private string price;
+
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public string Price { get => price; set => price = value; }
+
+        public Book() { }
+
+        public override string? ToString()
+        {
+            return $"Title: {Title}, Author: {Author}, Price: {Price}";
+        }
+    }
+}
---
+++ FileExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileExercise;
+using Newtonsoft.Json;
+using System.Text;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Files Exercise!");
+
+        FileManager fm = new FileManager (@"C:\Users\johan\source\repos\testitxt\TESTI.TXT");
+
+
+        try
+        {
+            string fileContent = fm.ReadWords();
+            Console.WriteLine(fileContent);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Tiedoston luku epäonnistui" + ex.Message);
+        }
+
+
+        List<Book> books = JsonConvert.DeserializeObject<List<Book>>(File.ReadAllText(@"C:\Users\johan\source\repos\Olio ohjelmointi\FileExercise\myBook.json", Encoding.UTF8));
+
+
+        foreach (Book b in books)
+        {
+            Console.WriteLine(b);
+        }
+
+
+
+    }
+        
+}
+
+
